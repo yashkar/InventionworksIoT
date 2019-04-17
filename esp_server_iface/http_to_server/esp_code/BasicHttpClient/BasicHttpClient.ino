977ff94385c887d0fd3881895727ff6c125f66b8
yash <yashkarundia@gmail.com>
2019-04-17 18:03:19 -0500

    Directory structure for esp-cloud interface

---
+++ esp_server_iface/http_to_server/esp_code/BasicHttpClient/BasicHttpClient.ino
@@ -0,0 +1,77 @@
+/**
+ * BasicHTTPClient.ino
+ *
+ *
+ */
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+
+#define USE_SERIAL Serial
+
+ESP8266WiFiMulti WiFiMulti;
+
+void setup() {
+
+    USE_SERIAL.begin(115200);
+   // USE_SERIAL.setDebugOutput(true);
+
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+
+    for(uint8_t t = 4; t > 0; t--) {
+        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
+        USE_SERIAL.flush();
+        delay(1000);
+    }
+
+    WiFi.mode(WIFI_STA);
+    WiFiMulti.addAP("EE-IOT-Platform-02", "****");//Ask admin for the password and enter here
+
+}
+long i;
+void loop() {
+    // wait for WiFi connection
+    //int i=0;
+    if((WiFiMulti.run() == WL_CONNECTED)) {
+
+        HTTPClient http;
+
+        USE_SERIAL.print("[HTTP] begin...\n");
+        // configure traged server and url
+        //http.begin("https://192.168.1.12/test.html", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
+        //i++;
+        i = random(300);
+        String s1 = "http://10.157.130.77/test.php?p=4&h=3&t=6";
+	//Randomize the data from here
+        //String s2 = "http://34.73.65.236/connect.php?value=";
+        //String s3 = s2 + i;
+
+        http.begin(s1); //HTTP
+        USE_SERIAL.print("[HTTP] GET...\n");
+        // start connection and send HTTP header
+        int httpCode = http.GET();
+
+        // httpCode will be negative on error
+        if(httpCode > 0) {
+            // HTTP header has been send and Server response header has been handled
+            USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+
+            // file found at server
+            if(httpCode == HTTP_CODE_OK) {
+                String payload = http.getString();
+                USE_SERIAL.println(payload);
+            }
+        } else {
+            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+
+        http.end();
+    }
+
+    delay(1000);
+}
